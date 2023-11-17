27bf59ed5958501ecc87962c1ac690fe6a4fdfc6
vincent.cheung <cheungchunwaivincent@gmail.com>
2023-11-18 00:14:31 +0800

    update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,19 +10,22 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path('about/', view=views.about_us, name='about_us'),
 
     # path for contact us view
+    path('contact/', view=views.contact_us, name='contact_us'),
 
     # path for registration
+    path('signup/', view=views.registration_request, name='registration'),
 
     # path for login
+    path('login/', view= views.login_request, name='login'),
 
     # path for logout
+    path('logout/', view= views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
-    path('about/', view=views.about_us, name='about_us'),
-    path('contact/', view=views.contact_us, name='contact_us'),
     # path for dealer reviews view
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for add a review view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -20,38 +20,81 @@ logger = logging.getLogger(__name__)
 # Create an `about` view to render a static about page
 # def about(request):
 # ...
+def about_us(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/about_us.html', context)
 
 
 # Create a `contact` view to return a static contact page
 #def contact(request):
+def contact_us(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('djangoapp:index')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'djangoapp:index')
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    logout(request)
+    # Optionally, you can add a success message or perform other actions after logout
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
-
+def registration_request(request):
+    context = {}
+    # If it is a GET request, just render the registration page
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    # If it is a POST request
+    elif request.method == 'POST':
+        # Get user information from request.POST
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.debug("{} is new user".format(username))
+        # If it is a new user
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            messages.error(request, 'User already exists.')
+            return redirect('djangoapp:index')
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
 
-def about_us(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/about_us.html', context)
 
-def contact_us(request):
-    context = {}
-    if request.method == "GET":
-        return render(request, 'djangoapp/contact.html', context)
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):

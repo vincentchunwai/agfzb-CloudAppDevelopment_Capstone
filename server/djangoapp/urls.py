b7ac121eed1429a4ddcfb691bf7082501c06b75c
vincent.cheung <cheungchunwaivincent@gmail.com>
2023-11-18 20:59:13 +0800

    update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,28 +5,28 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
+                  # route is a string contains a URL pattern
+                  # view refers to the view function
+                  # name the URL
 
-    # path for about view
-    path('about/', view=views.about_us, name='about_us'),
+                  # path for about view
+                  path('about/', view=views.about_us, name='about_us'),
 
-    # path for contact us view
-    path('contact/', view=views.contact_us, name='contact_us'),
+                  # path for contact us view
+                  path('contact/', view=views.contact_us, name='contact_us'),
 
-    # path for registration
-    path('signup/', view=views.registration_request, name='registration'),
+                  # path for registration
+                  path('signup/', view=views.registration_request, name='registration'),
 
-    # path for login
-    path('login/', view= views.login_request, name='login'),
+                  # path for login
+                  path('login/', view=views.login_request, name='login'),
 
-    # path for logout
-    path('logout/', view= views.logout_request, name='logout'),
+                  # path for logout
+                  path('logout/', view=views.logout_request, name='logout'),
 
-    # path for dealer reviews view
-    path(route='', view=views.get_dealerships, name='index'),
+                  # path for dealer reviews view
+                  path(route='', view=views.get_dealerships, name='index'),
 
-    # path for add a review view
+                  # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -9,7 +9,7 @@ from django.contrib import messages
 from datetime import datetime
 import logging
 import json
-
+from .restapis import get_dealers_from_cf
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -90,15 +90,21 @@ def registration_request(request):
             return redirect('djangoapp:index')
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://cheungchunwa-3000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
